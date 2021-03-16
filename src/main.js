b53729d5b4555913fd5910466578a54a37db8ffa
Nick Hennessy <njhnny@gmail.com>
2021-03-15 19:32:37 -0700

    change units to farenheit

--- src/main.js
+++ src/main.js
@@ -6,25 +6,24 @@ $(document).ready(function() {
   $('#weatherLocation').click(function() {
     const city = $('#location').val();
     $('#location').val("");
-
+    
     let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=af015b9cd2d1ae1a328804125dc2b1c0`;
-
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.API_KEY}`;
+    
     request.onreadystatechange = function() {
       
       if (this.readyState === 4 && this.status === 200) {
-        debugger;
         const response = JSON.parse(this.responseText);
         getElements(response);
       }
     };
-
+    
     request.open("GET", url, true);
     request.send();
-
+    
     function getElements(response) {
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+      $('.showTemp').text(`The temperature in Farenheit is ${response.main.temp} degrees.`);
     }
   });
 });
